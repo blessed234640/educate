75ad13ec4649bfdf5267668775a8ebbd06a4e3a7
blessed234640 <Arturazimov200577@gmail.com>
2025-12-01 04:52:00 +0500

    update models and add new field.py for sort models

---
+++ educa/courses/fields.py
@@ -0,0 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+
+class OrderField(models.PositiveIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        self.for_fields = for_fields
+        super().__init__(*args, **kwargs) 

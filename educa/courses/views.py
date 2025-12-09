db24942d6bc9f767c10a652e4b6217513619c3d3
blessed234640 <Arturazimov200577@gmail.com>
2025-12-09 17:19:51 +0500

    add cache memcached, update views.py application of courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,20 +1,23 @@
+from importlib.machinery import all_suffixes
+
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.apps import apps
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.cache import cache
+from django.db.models import Count
+from django.forms.models import modelform_factory
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-
-from .models import Course
-from django.shortcuts import get_object_or_404, redirect
-from django.views.generic.base import TemplateResponseMixin, View
-from .forms import ModuleFormSet
-from django.apps import apps
-from django.forms.models import modelform_factory
-from .models import Module, Content
-from  braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-from django.db.models import Count
-from .models import Subject
-from django.views.generic.detail import DetailView
 from students.forms import CourseEnrollForm
+
+from .forms import ModuleFormSet
+from .models import Content, Course, Module, Subject
+
+
 class OwnerMixin:
     def get_queryset(self):
         qs = super().get_queryset()
@@ -56,75 +59,61 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
 
 
 class CourseModuleUpdateView(TemplateResponseMixin, View):
-    template_name = 'courses/manage/module/formset.html'
+    template_name = "courses/manage/module/formset.html"
     course = None
 
     def get_formset(self, data=None):
         return ModuleFormSet(instance=self.course, data=data)
-    
+
     def dispatch(self, request, pk):
-        self.course = get_object_or_404(
-            Course, id=pk, owner=request.user
-        )
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
         return super().dispatch(request, pk)
-    
+
     def get(self, request, *args, **kwargs):
         formset = self.get_formset()
-        return self.render_to_response(
-            {'course': self.course, 'formset': formset}
-        )
-    
+        return self.render_to_response({"course": self.course, "formset": formset})
+
     def post(self, request, *args, **kwargs):
         formset = self.get_formset(data=request.POST)
         if formset.is_valid():
             formset.save()
-            return redirect('manage_course_list')
-        return self.render_to_response(
-            {'course': self.course, 'formset': formset}
-        )
+            return redirect("manage_course_list")
+        return self.render_to_response({"course": self.course, "formset": formset})
 
 
 class ContentCreateUpdateView(TemplateResponseMixin, View):
     module = None
     model = None
     obj = None
-    template_name = 'courses/manage/content/form.html'
+    template_name = "courses/manage/content/form.html"
 
     def get_model(self, model_name):
-        if model_name in ['text', 'video', 'image', 'file']:
-            return apps.get_model(
-                app_label='courses', model_name=model_name
-            )
+        if model_name in ["text", "video", "image", "file"]:
+            return apps.get_model(app_label="courses", model_name=model_name)
         return None
-    
+
     def get_form(self, model, *args, **kwargs):
         Form = modelform_factory(
-            model, exclude=['owner', 'order', 'created', 'updated']
+            model, exclude=["owner", "order", "created", "updated"]
         )
         return Form(*args, **kwargs)
-    
+
     def dispatch(self, request, module_id, model_name, id=None):
         self.module = get_object_or_404(
             Module, id=module_id, course__owner=request.user
         )
         self.model = self.get_model(model_name)
         if id:
-            self.obj = get_object_or_404(
-                self.model, id=id, owner=request.user
-            )
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
         return super().dispatch(request, module_id, model_name, id)
-    
+
     def get(self, request, module_id, model_name, id=None):
         form = self.get_form(self.model, instance=self.obj)
-        return self.render_to_response(
-            {'form': form, 'object': self.obj}
-        )
-    def post(self, request, module_id, model_name , id=None):
+        return self.render_to_response({"form": form, "object": self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
         form = self.get_form(
-            self.model,
-            instance=self.obj,
-            data=request.POST,
-            files=request.FILES
+            self.model, instance=self.obj, data=request.POST, files=request.FILES
         )
         if form.is_valid():
             obj = form.save(commit=False)
@@ -132,80 +121,76 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
             obj.save()
             if not id:
                 Content.objects.create(module=self.module, item=obj)
-            return redirect('module_content_list', self.module.id)
-        return self.render_to_response(
-            {'form': form, 'object': self.obj}
-        )
+            return redirect("module_content_list", self.module.id)
+        return self.render_to_response({"form": form, "object": self.obj})
 
 
 class ContentDeleteView(View):
     def post(self, request, id):
-        content = get_object_or_404(
-            Content, id=id, module__course__owner=request.user
-        )
+        content = get_object_or_404(Content, id=id, module__course__owner=request.user)
         module = content.module
         content.item.delete()
         content.delete()
-        return redirect('module_content_list', module.id)
+        return redirect("module_content_list", module.id)
 
 
 class ModuleContentListView(TemplateResponseMixin, View):
-    template_name = 'courses/manage/module/content_list.html'
+    template_name = "courses/manage/module/content_list.html"
 
     def get(self, request, module_id):
-        module = get_object_or_404(
-            Module, id=module_id, course__owner=request.user
-        )
-        return self.render_to_response({'module': module})
-    
+        module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        return self.render_to_response({"module": module})
+
 
 class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
     def post(self, request):
         for id, order in self.request_json.items():
-            Module.objects.filter(
-                id=id, course__owner=request.user
-            ).update(order=order)
-        return self.render_json_response({'saved': 'OK'})
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({"saved": "OK"})
 
 
 class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
     def post(self, request):
         for id, order in self.request_json.items():
-            Content.objects.filter(
-                id=id, module__course__owner=request.user
-            ).update(order=order)
-        return self.render_json_response({'saved': 'OK'})
-    
+            Content.objects.filter(id=id, module__course__owner=request.user).update(
+                order=order
+            )
+        return self.render_json_response({"saved": "OK"})
+
 
 class CourseListView(TemplateResponseMixin, View):
     model = Course
-    template_name = 'courses/course/list.html'
+    template_name = "courses/course/list.html"
+
     def get(self, request, subject=None):
-        subjects = Subject.objects.annotate(
-            total_courses = Count('courses')
-        )
-        courses = Course.objects.annotate(
-            total_modules=Count('modules')
-        )
+        subjects = cache.get("all_subjects")
+        if not subjects:
+            subjects = Subject.objects.annotate(total_courses=Count("courses"))
+            cache.set("all_subjects", subjects)
+
+        all_courses = Course.objects.annotate(total_modules=Count("modules"))
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
-            courses = courses.filter(subject=subject)
+            key = f"subject_{subject.id}_courses"
+            courses = cache.get(key)
+            if not courses:
+                courses = all_courses.filter(subject=subject)
+                cache.set(key, courses)
+            else:
+                courses = cache.get("all_courses")
+                if not courses:
+                    courses = all_courses
+                    cache.set("all_courses", courses)
         return self.render_to_response(
-            {
-                'subjects': subjects,
-                'subject': subject,
-                'courses': courses
-            }
+            {"subjects": subjects, "subject": subject, "courses": courses}
         )
-    
+
+
 class CourseDetailView(DetailView):
     model = Course
-    template_name = 'courses/course/detail.html'
+    template_name = "courses/course/detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['enroll_form'] = CourseEnrollForm(
-            initial={'course': self.object}
-
-        )
+        context["enroll_form"] = CourseEnrollForm(initial={"course": self.object})
         return context
--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+
 from django.urls import reverse_lazy
-LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+LOGIN_REDIRECT_URL = reverse_lazy("student_course_list")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -18,12 +20,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'students.apps.StudentsConfig',
-    'braces',
-    'embed_video',
+    "students.apps.StudentsConfig",
+    "braces",
+    "embed_video",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -82,9 +86,21 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]

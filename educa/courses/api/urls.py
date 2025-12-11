5f30513ce367ced554e4e919fa769012acdfcdc6
blessed234640 <Arturazimov200577@gmail.com>
2025-12-12 00:31:19 +0500

    create API Update our files, and now our api is working

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -1,14 +1,18 @@
 from django.urls import include, path
-from . import views
 from rest_framework import routers
 
-app_name = 'courses'
+from . import views
+
+app_name = "courses"
 
 router = routers.DefaultRouter()
-router.register('courses', views.CourseViewSet)
-router.register('subjects', views.SubjectViewSet)
+router.register("courses", views.CourseViewSet)
+router.register("subjects", views.SubjectViewSet)
 urlpatterns = [
     # path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
     # path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    # path(
+    #     "courses/<pk>/enroll/", views.CourseEnrollView.as_view(), name="course_enroll"
+    # ),
 ]
